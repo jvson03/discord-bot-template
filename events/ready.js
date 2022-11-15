9c22b293ac872af3f8cdd07c087123126df553ca
Jvson03 <jvsonlamaj@gmail.com>
2022-11-15 20:29:00 +0100

    fix: useless comments

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,6 @@ module.exports = {
 	once: true,
 	execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
-		// We set the bot's activity & status.
 		// Set the client user's presence
 		client.user.setPresence({ activities: [{ name: 'Redwood V' }], status: 'idle' });
 	},
